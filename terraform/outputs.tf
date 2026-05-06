fbaca1bcd36e1c3cde5eb713acb031bfceb6210f
shriganeshchoudhari <ganesh@mail.com>
2026-05-06 22:23:33 +0530

    feat: add Terraform outputs for CI instance IP and Jenkins IAM credentials

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,12 @@
 output "ci_public_ip" {
   value = aws_instance.ci.public_ip
 }
+
+output "jenkins_access_key_id" {
+  value = aws_iam_access_key.jenkins_key.id
+}
+
+output "jenkins_secret_access_key" {
+  value     = aws_iam_access_key.jenkins_key.secret
+  sensitive = true
+}
